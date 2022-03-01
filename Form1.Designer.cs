a88335ab9fa84267fc558468172ac8299747ed80
odruenne <druenneoceane@gmail.com>
2022-03-01 10:34:21 +0100

    avancement du projet

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.txtProposition = new System.Windows.Forms.TextBox();
             this.pnlMot = new System.Windows.Forms.Panel();
             this.btnQuitter = new System.Windows.Forms.Button();
+            this.btnRecommencer = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblMotATrouver
@@ -49,7 +50,7 @@
             // 
             this.lblProposition.AutoSize = true;
             this.lblProposition.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.lblProposition.Location = new System.Drawing.Point(469, 50);
+            this.lblProposition.Location = new System.Drawing.Point(685, 50);
             this.lblProposition.Name = "lblProposition";
             this.lblProposition.Size = new System.Drawing.Size(156, 22);
             this.lblProposition.TabIndex = 1;
@@ -57,21 +58,23 @@
             // 
             // txtProposition
             // 
-            this.txtProposition.Location = new System.Drawing.Point(642, 52);
+            this.txtProposition.Location = new System.Drawing.Point(847, 52);
             this.txtProposition.Name = "txtProposition";
             this.txtProposition.Size = new System.Drawing.Size(100, 20);
             this.txtProposition.TabIndex = 2;
+            this.txtProposition.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtProposition_KeyPress);
             // 
             // pnlMot
             // 
+            this.pnlMot.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pnlMot.Location = new System.Drawing.Point(46, 138);
             this.pnlMot.Name = "pnlMot";
-            this.pnlMot.Size = new System.Drawing.Size(696, 100);
+            this.pnlMot.Size = new System.Drawing.Size(901, 100);
             this.pnlMot.TabIndex = 3;
             // 
             // btnQuitter
             // 
-            this.btnQuitter.Location = new System.Drawing.Point(701, 415);
+            this.btnQuitter.Location = new System.Drawing.Point(872, 415);
             this.btnQuitter.Name = "btnQuitter";
             this.btnQuitter.Size = new System.Drawing.Size(75, 23);
             this.btnQuitter.TabIndex = 4;
@@ -79,11 +82,22 @@
             this.btnQuitter.UseVisualStyleBackColor = true;
             this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
             // 
+            // btnRecommencer
+            // 
+            this.btnRecommencer.Location = new System.Drawing.Point(763, 415);
+            this.btnRecommencer.Name = "btnRecommencer";
+            this.btnRecommencer.Size = new System.Drawing.Size(103, 23);
+            this.btnRecommencer.TabIndex = 5;
+            this.btnRecommencer.Text = "Recommencer";
+            this.btnRecommencer.UseVisualStyleBackColor = true;
+            this.btnRecommencer.Click += new System.EventHandler(this.btnRecommencer_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(975, 450);
+            this.Controls.Add(this.btnRecommencer);
             this.Controls.Add(this.btnQuitter);
             this.Controls.Add(this.pnlMot);
             this.Controls.Add(this.txtProposition);
@@ -91,6 +105,7 @@
             this.Controls.Add(this.lblMotATrouver);
             this.Name = "Form1";
             this.Text = "Jeu du pendu";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,6 +118,7 @@
         private System.Windows.Forms.TextBox txtProposition;
         private System.Windows.Forms.Panel pnlMot;
         private System.Windows.Forms.Button btnQuitter;
+        private System.Windows.Forms.Button btnRecommencer;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -31,22 +31,68 @@ namespace JeuDuPendu
             return pos;
         }
 
+        public string choixDuMot()
+        {
+            int endroit = generePlace();
+            string motChoisi = tabMots[endroit];
+            return motChoisi;
+        }
+
         public string recupererMot()
         {
             int position = generePlace();
             string motADeviner = tabMots[position];
+            char lettres = motADeviner[0];
+            char derniereLettre = motADeviner[motADeviner.Length - 1];
+            int left = 20;
+            int top = 10;
+            int nbElements = motADeviner.Length;
+
+            for (int i = 0; i < nbElements; i++)
+            {
+                TextBox txtBox = new TextBox();
+                txtBox.Tag = i;
+                int emplacement = (int)txtBox.Tag;
+                if (emplacement == 0)
+                {
+                    txtBox.Text = lettres.ToString();
+                }
+
+                if (emplacement == nbElements - 1)
+                {
+                    txtBox.Text = derniereLettre.ToString();
+                }
+
+                txtBox.Size = new System.Drawing.Size(50, 10);
+                txtBox.Left = left;
+                txtBox.Top = top;
+                pnlMot.Controls.Add(txtBox);
+                left += 80;
+            }
             return motADeviner;
         }
 
-        public void constructionMot()
+        private void Form1_Load(object sender, EventArgs e)
         {
-            string motChoisi = recupererMot();
-            char[] lettres = new char[motChoisi.Length];
-            motChoisi.Split();
-            for (int i = 0; i < motChoisi.Length; i++)
-            {
-                MessageBox.Show(motChoisi.Split().ToString());
+            recupererMot();
 
+
+        }
+
+        private void btnRecommencer_Click(object sender, EventArgs e)
+        {
+            foreach (TextBox txt in pnlMot.Controls.OfType<TextBox>())
+            {
+                txt.Clear();
+            }
+
+        }
+
+        private void txtProposition_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 10 || e.KeyChar == 13)
+            {
+                e.Handled = false;
             }
 
         }
